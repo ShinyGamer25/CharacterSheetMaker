6f6ecb9626008038b8b3ddd177d47260c001a662
ShinyGamer25 <oisinscuffins39@gmail.com>
2025-03-21 21:45:32 +0000

    weapon select and other gender
    added weapons selection for fighter class and a text box for other genders

--- Javascript/Code.js
+++ Javascript/Code.js
@@ -1,11 +1,34 @@
 function classSelected(){
     console.log("Selection changed")
-    let selectedClass = document.querySelector("#class").value;
+    let selectedClass = document.querySelector("#classSelector").value;
     if(selectedClass == "wizard"){
         document.querySelector("#chooseSpell").classList.remove("hide");
+        document.querySelector("#chooseWeapon").classList.add("hide");
     }
 
     if(selectedClass == "fighter"){
         document.querySelector("#chooseSpell").classList.add("hide");
+        document.querySelector("#chooseWeapon").classList.remove("hide");
     }
+
+    if(selectedClass == "rogue"){
+        document.querySelector("#chooseSpell").classList.add("hide");
+        document.querySelector("#chooseWeapon").classList.add("hide");
+    }
+}
+
+function genderSelected(){
+    console.log("Gender Selected")
+    let genderSelected = document.querySelector("#genderSelector").value;
+    if(genderSelected == "other"){
+        document.querySelector("#otherGender").classList.remove("hide");
+    }
+
+    else{
+        document.querySelector("#otherGender").classList.add("hide");
+    }
+}
+
+function raceSelected(){
+    console.log("Race selected")
 }
